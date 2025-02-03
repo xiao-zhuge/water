c2acd8eea4ec736fef551ab661c222adc701fbe6
zhuguanqun <773658446@qq.com>
2025-02-03 22:43:11 +0800

    测试

---
+++ assets/CSS-1aa394ff.js
@@ -0,0 +1,79 @@
+import{S as l}from"./StudyMenu-5e12a384.js";import{u as d}from"./useMarked-b2d6c03a.js";import"./jspdf.es.min-e15db21f.js";import{_ as a,r as i,a as p,p as f,e as h,b as u,c as m,d as s,f as _}from"./index-d8c2bca0.js";import"./_commonjsHelpers-042e6b4d.js";const x=`
+## 尺寸的百分比
+
+### 参考系
+百分比是一个相对单位，其相对于元素的参考系。
+- 普通元素的参考系为**父元素的内容区域**
+- 绝对(固定)定位元素的参考系为**父元素中第一个定位元素的padding区域**
+
+### 常见百分比
+1. width: 参考系的宽度
+2. height：参考系的高度（参考系的高度受本身影响时，设置无效）
+3. padding：参考系的宽度
+4. border: 参考系的宽度
+5. margin: 参考系的宽度
+`,y=`
+## 属性值计算过程
+    样式表：用户声明的属性+浏览器默认属性
+### 一. 确定声明值
+    参考样式表中没有声明冲突的属性，作为 css 属性值
+### 二. 层叠冲突
+    对有冲突的声明使用层叠规则
+##### 1.比较重要性
+        用户声明 > 浏览器默认样式
+##### 2.比较特殊性
+        权重
+##### 3.比较原次序
+        后面的覆盖前面的
+### 三. 使用继承
+    对仍然没有值的属性、若可以继承，则继承父元素的值
+### 四. 使用默认值
+    对仍然没有值的属性，使用默认值
+**注意：设置值为 inherit（color:inherit）,直接使用继承，绕过第三步**
+`,k=`
+## 伪类选择器
+
+- E:not(s)  选中的E中 不包括s选中的元素
+- E:root  根元素选择器,在html中等价于 html标签选择器
+- E:target 点击a标签，跳转到响应锚地后的元素
+
+- E:first-child  所有选中的元素的第一个孩子元素
+- E:last-child 选中元素的最后一个孩子元素
+- E:only-child  没有任何兄弟元素的元素
+- E:nth-child(n)  第n个子元素
+- E:nth-last-child  倒数第n个子元素
+
+- E:first-of-type  第一个该类型的元素
+- E:last-of-type  最后一个该类型的元素
+- E:only-of-type  只有该类型元素的元素
+- E:nth-of-type(n)  第n个该类型的元素
+- E:nth-of-last-type(n)  倒数第n个该类型的元素
+
+- E:empty  空内容的元素
+- E:checked  checked的元素
+- E:enabled  enabled的元素
+- E:disabled  disabled的元素
+- E:read-only  不可被编辑的元素
+- E:read-write  可被编辑的元素
+`,E=""+new URL("flexShrink1-2af1cc18.png",import.meta.url).href,S=""+new URL("flexShrink2-2f491ddd.png",import.meta.url).href,b=`
+## flex-shrink
+> 当空间不够时设置了flex-shrink的元素将被缩小，默认为0不缩小
+#### 1. 父宽600，自宽300，flex-shrink: 1 1 1,
+**子元素宽度都被缩小到了200**
+![](${E})
+#### 2. 父宽600，box1 box2是200，box3是400，flex-shrink：1 1 3
+**子元素缩放后的宽度为175 175 250, 而不是160 160 280** 
+![](${S})
+\`\`\`shell
+# 需要缩放的宽度的 200
+# 算法：加权平均数
+# 1. 加权
+# 200 * 1 + 200 * 1 + 400 * 3 = 1600 
+# 2. 求平均值
+# box1 box2
+# (200 * 1) / 1600 * 200 = 25px
+# box3
+# (400 * 3) / 1600 * 200 = 150px
+\`\`\`
+**加权时候，200不是盒模型宽度，而是真实内容宽度**
+`;const g={sizePercentage:x,styleCompute:y,pseudoClasses:k,flexShrink:b},C={components:{StudyMenu:l},setup(){const n=i("sizePercentage"),e=p([{key:"sizePercentage",title:"尺寸的百分比"},{key:"styleCompute",title:"属性计算"},{key:"pseudoClasses",title:"伪类选择器"},{key:"flexShrink",title:"flex-shrink"}]),o=f(()=>d(g[n.value]));return{menu:e,curSelected:n,mdContent:o}}},v={class:"page study-page"},M={class:"content"},w=["innerHTML"];function L(n,e,o,t,R,z){const r=h("StudyMenu");return u(),m("div",v,[s("div",M,[s("div",{class:"markdown-body",innerHTML:t.mdContent},null,8,w)]),_(r,{menu:t.menu,curSelected:t.curSelected,"onUpdate:curSelected":e[0]||(e[0]=c=>t.curSelected=c)},null,8,["menu","curSelected"])])}const T=a(C,[["render",L],["__scopeId","data-v-710ce5a4"]]);export{T as default};
---
+++ assets/Server-6a14e185.js
@@ -0,0 +1,161 @@
+import{S as a}from"./StudyMenu-5e12a384.js";import{u as i}from"./useMarked-b2d6c03a.js";import{_ as p,r as u,a as l,p as d,e as m,b as k,c as y,d as r,f as h}from"./index-d8c2bca0.js";import"./_commonjsHelpers-042e6b4d.js";const g=`
+## Cookie
+
+### 一. 组成
+- key: 键
+- value: 值
+- domain: 域，表示这个cookie属于哪个网站，比如baidu.com表示这个cookie属于baidu.com这个网站
+- path: 路径，表示这个cookie属于这个网站的哪个路径，比如/name, 那么该路径及其子路径都会携带该cookie
+- secure: 是否使用安全传输，如果为true，那么只有https请求会携带该cookie
+- expire: 过期时间 GMT格式，与max-age只能设置一个
+- max-age: 过期时间，相对时间，比如设置1000会认为1000s后过期，并转成GMT时间存在expire中
+- httponly: 是否只用于http请求，如果为true，js将无法获取该cookie
+
+### 二. 请求携带条件
+1. 没有cookie
+2. cookie中的域和请求是匹配的， **cookie不在乎端口**
+> baidu.com可匹配baidu.com、www.baidu.com、blogs.baidu.com
+3. path和请求的paht可匹配
+> /name可匹配 /name、/name/1
+4. 验证cookie的安全传输
+
+### 三. 设置cookie
+#### 1. 服务端
+\`\`\`JS
+// set-cookie: 键=值; path=?; domain=?; expire=?; max-age=?; secure; httponly
+res.setHeader('set-cookie', 'token=1234; path=/; max-age=1000; httponly=true')
+\`\`\`
+
+#### 2. 客户端
+\`\`\`JS
+document.cookie = "键=值; path=?; domain=?; expire=?; max-age=?; secure";
+\`\`\`
+
+### 四. 删除cookie
+设置**max-age=-1**，那么会认为马上过期，浏览器会在下次请求时候自动删除该cookie
+
+### 五. 多端应用
+因为浏览器会自动携带cookie，但是其他设备不会，所以其他设备可以在请求头中加上身份信息
+\`\`\`JS
+res.header('authorization', 6)
+\`\`\`
+
+> cookie中间键: cookie-parser
+`,f=`
+## 使用node的crypto模块实现对称加密
+
+\`\`\`js
+const crypto = require('crypto');
+// console.log(crypto.getCiphers()); // 获取所有支持的加密算法
+
+// 128为私钥
+const secret = Buffer.from('x8wggaph8y5aikoo');
+
+module.exports = function () {
+    // 准备一个随机向量：iv，方式私钥泄漏
+    const iv = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+
+    return {
+        // 加密
+        encrypt(str) {
+            const cry = crypto.createCipheriv('aes-128-cbc', secret, iv); // 使用 aes-128-cbc 算法加密
+            let result = cry.update(str, 'utf-8', 'hex'); // 输入的编码格式是utf-8, 输出的是 hex
+            result += cry.final('hex');
+            return result;
+        },
+        // 解密
+        decrypt(str) {
+            const decry = crypto.createDecipheriv('aes-128-cbc', secret, iv);
+            let result = decry.update(str, 'hex', 'utf-8');
+            result += decry.final('utf-8');
+            return result;
+        },
+    };
+};
+\`\`\`
+`,_=""+new URL("cors-06baaf85.png",import.meta.url).href,x=`
+## 跨域
+
+## jsonp
+
+#### 1. 实现方式
+
+\`\`\`js
+///////// 服务端
+const router = express.Router();
+
+router.get('/', async(req, res) => {
+    // 获取完数据后的特殊处理
+    // 将数据转换成json格式，并放到一个函数中
+    const json = JSON.stringify(result);
+    const script = \`callback(\${json})\`;
+    res.header('content-type', 'application/javascript').send(script)
+})
+
+///////// 客户端
+
+// 将请求作为script元素的src属性，添加到document中
+
+function jsonp(url) {
+    const script = document.createElement('script');
+    script.src = url;
+    document.body.appendChild(script);
+    script.onload = function() {
+        script.remove();
+    }
+}
+
+function callback(data) {
+    // 因为服务端将结果包装在了callback，所以当浏览器加载script后自动运行返回的代码，也就是运行callback函数
+    console.log(data)
+}
+
+jsonp(http://172.168.0.1:5001/api/userlist)
+\`\`\`
+
+#### 2. 缺点
+- 只能使用get请求，因为script标签就是get请求
+- 会改变服务器返回格式
+
+## CORS
+
+### 一. 简单请求
+
+#### 简单请求判断
+1. 请求方式是 get post header
+2. 请求头仅包含安全字段
+> 常见安全字段: Accept、Accept-Language、Content-Language、Content-Type、DPR、Downlink、Save-Data、Viewport-Width、Width
+3. 请求头如果包含content-type，只能是下面几个值之一
+> text/plain  multipary/form-data  application/x-www-form-unlencoded
+
+#### 简单请求交互
+1. 请求头中会自动添加Origin字段，来表示我的源地址路径
+2. 服务器在响应头中加入 **Access-Control-Allow-Origin**
+
+### 二. 需要预检的请求
+1. 浏览器发送预检请求，询问服务器是否允许
+    - 请求方法 options
+    - 无请求体
+    - 请求头中包含字段
+        - Origin: 请求源
+        - Access-Control-Request—Method: 后续真实请求的请求方法
+        - Access-Control-Request-Headers: 真实请求会改动的请求头
+        
+2. 服务器允许
+    - Access-Control-Request-Origin: 允许的源
+    - Access-Control-Request-Methods: 允许的请求方法
+    - Access-Control-Request-Headers: 允许的请求头
+    - [Access-Control-Max-Age: 1000]: 1000s之内不用再访问是否允许
+
+3. 浏览器发送真实请求
+
+![](${_})
+
+#### 三. 带身份信息(cookie)的请求
+
+在请求头中添加：Access-Control-Allow-Credentials: true
+
+> 默认情况下，js只能获取部分基础响应头，如果要访问其他响应头内容，需要服务器添加白名单: **Access-Control-Expose-Headers: a,b,c**
+
+> 中间键：cors
+`;const C={cookie:g,symmetricEncryption:f,crossDomain:x},S={components:{StudyMenu:a},setup(){const o=u("cookie"),e=l([{key:"cookie",title:"Cookie"},{key:"symmetricEncryption",title:"对称加密"},{key:"crossDomain",title:"跨域"}]),c=d(()=>i(C[o.value]));return{menu:e,curSelected:o,mdContent:c}}},b={class:"page study-page"},v={class:"content"},A=["innerHTML"];function w(o,e,c,t,M,R){const s=m("StudyMenu");return k(),y("div",b,[r("div",v,[r("div",{class:"markdown-body",innerHTML:t.mdContent},null,8,A)]),h(s,{menu:t.menu,curSelected:t.curSelected,"onUpdate:curSelected":e[0]||(e[0]=n=>t.curSelected=n)},null,8,["menu","curSelected"])])}const H=p(S,[["render",w],["__scopeId","data-v-cab24208"]]);export{H as default};
